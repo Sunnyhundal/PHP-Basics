27c620be90ab2376f7b74fcf2b4ac70d101ed2f8
Sunny Hundal <amandip.h@gmail.com>
2024-01-14 23:49:18 -0800

    initial commit

---
+++ basics.php
@@ -0,0 +1,33 @@
+<!-- Basic PHP syntax and functions -->
+
+<?php
+echo "Hello World!";
+?>
+
+// Echo is a function that prints out the string that is passed to it.
+// Note: in php, all statements must end with a semicolon.
+
+// Variables
+<?php
+$my_variable = "Hello World!";
+echo $my_variable;
+?>
+
+// Variables in php start with a dollar sign ($).
+// Variables are assigned with the = operator.
+// Variables can be printed with the echo function.
+
+// Data Types
+<?php
+$my_string = "Hello World!";
+$my_number = 42;
+$my_array = array("red", "green", "blue");
+$my_bool = true;
+?>
+
+// Strings are sequences of characters, like "Hello World!".
+// Numbers can be integers (whole numbers) or floats (decimals).
+// Arrays are ordered lists of values.
+// Booleans are either true or false.
+
+//php doesn't require you to declare variables type before assigning them values.
